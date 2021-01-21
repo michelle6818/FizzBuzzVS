3786794b24cb2657f09de165890eb405255a6af6
michelle6818 <75517393+michelle6818@users.noreply.github.com>
2021-01-21 15:40:00 -0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using FizzBuzz.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FizzBuzz.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //GET
+        public IActionResult Index()
+        {
+            //Object initialization
+            var model = new FizzandBuzz();
+            return View(model);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(FizzandBuzz input)
+        {
+            //Input numbers
+            int firstNum = input.FirstNum;
+            int secondNum = input.SecondNum;
+            var test = input.IsMultiple;
+
+            for (int loop = firstNum; loop <= secondNum; loop++)
+            {
+                int fizz = loop % 3;
+                int buzz = loop % 5;
+                
+                if(fizz == 0 && buzz == 0)
+                {
+                    input.IsMultiple = true;
+                    input.Message += " FizzBuzz";
+                }
+                else if(fizz == 0)
+                {
+                    input.IsMultiple = true;
+                    input.Message += " Fizz";
+                }
+                else if(buzz == 0)
+                {
+                    input.IsMultiple = true;
+                    input.Message += " Buzz";
+                }
+                else
+                {
+                    input.Message += $" {loop}";
+                }
+
+            }
+
+                return View(input);
+        }
+
+        private IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/FizzandBuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Models
+{
+    public class FizzandBuzz
+    {
+        //User inputs 2 numbers
+        public int FirstNum { get; set; }
+        public int SecondNum { get; set; }
+
+        //Multiple of 3, 5, or 15
+        public string Fizz { get; set; }
+        public string Buzz { get; set; }
+        public string FizzBuzz { get; set; }
+
+        //If not a multiple
+        public bool IsMultiple { get; set; }
+
+        //output
+        public string Message { get; set; }
+
+
+    }
+}
